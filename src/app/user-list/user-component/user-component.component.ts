18ae7cd159ecf030bd005d9d09a0349e0723a689
lphilippe-source <philippelmr@gmail.com>
2020-11-13 19:56:48 +0100

    wip

--- src/app/user-list/user-component/user-component.component.ts
+++ src/app/user-list/user-component/user-component.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { UserService } from '../user.service';
+import { UserService, User } from '../user.service';
 
 @Component({
   selector: 'app-user-component',
@@ -7,10 +7,15 @@ import { UserService } from '../user.service';
   styleUrls: ['./user-component.component.css']
 })
 export class UserComponentComponent implements OnInit {
-
+user={
+  id:0,
+  wallet:0,
+}
 users:[]
 wallet:number = 0
-  constructor(private UserService: UserService) { }
+// addCredit:number = 0
+id:number
+  constructor(protected UserService: UserService) { }
 
   ngOnInit(): void {
     this.getList()
@@ -19,10 +24,20 @@ wallet:number = 0
   getList(){
     this.UserService.getUserList().subscribe((list:[])=>{
       this.users=list
-      console.log(this.users)
+      // console.log(this.users)
     })
   }
   editUser(e){
     this.wallet=e
+  }
+  send(){
+    this.UserService.sendValueWallet
+    
+  }
+  submitWallet(wallet,id){
+    this.user.wallet = wallet
+    this.user.id = id
+    this.send(this.user,this.user)
+    console.log(id)
   }
 }
--- src/app/user-list/user.service.ts
+++ src/app/user-list/user.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+export interface User{
+  id:number
+  wallet:number
+}
 const API:string = "http://localhost:8080/lunchtime/user/findall"
-const API2:string =""
+const API2:string ="http://localhost:8080/lunchtime/user/credit/"
 @Injectable({
   providedIn: 'root'
 })
@@ -10,12 +13,18 @@ export class UserService {
 
   
 
+
   constructor(private http: HttpClient) { }
 
+  getUrl(){
 
+  }
   getUserList(){
     return this.http.get(API)
   }
 
+  sendValueWallet(user:User, wallet){
+    return this.http.post(API2 +user.id+"?amout="+user.wallet, wallet)
+  }
 }
 
