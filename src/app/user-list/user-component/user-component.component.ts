b52fcf003825abbdf1aca77a016ea400178f2099
lphilippe-source <philippelmr@gmail.com>
2020-12-01 20:57:53 +0100

    mise a jour du dernier TP fini

--- src/app/user-list/user-component/user-component.component.ts
+++ src/app/user-list/user-component/user-component.component.ts
@@ -1,43 +1,92 @@
-import { Component, OnInit } from '@angular/core';
-import { UserService, User } from '../user.service';
 
+import { Component, EventEmitter, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
+
+interface Body {
+id:number
+wallet:number
+firstname:string
+name:string
+}
 @Component({
   selector: 'app-user-component',
   templateUrl: './user-component.component.html',
   styleUrls: ['./user-component.component.css']
 })
 export class UserComponentComponent implements OnInit {
+  isValid:any = false
 user={
   id:0,
   wallet:0,
 }
-users:[]
-wallet:number = 0
-// addCredit:number = 0
-id:number
+// Champs à afficher: name, firstname, wallet
+
+// L'application doit permettre d'augmenter ou de diminuer le montant du "wallet"
+// en effectuant les requêtes ajax correspondantes et afficher un message dans 
+// l'interface afin d'informer l'utilisateur de l'issue de sa requête
+errors?:any
+message?:string
+users:any[]
+wallet?:number
+id ?: number
+body: Body=
+  { 
+    id: 0, wallet: 0, firstname:"", name:""
+  }
+
   constructor(protected UserService: UserService) { }
 
   ngOnInit(): void {
     this.getList()
+    // besoin pour actualiser le bouton apres l'affichage de la list car une variable n'est pas encore créer.
+    this.changeValid()
   }
-
+  changeValid(){
+    this.isValid = true
+  }
   getList(){
     this.UserService.getUserList().subscribe((list:[])=>{
       this.users=list
-      // console.log(this.users)
+      console.log(this.users)
     })
   }
-  editUser(e){
+  eraseMessage(){
+    this.message=null
+    this.errors=null
+  }
+  editUser(e:number,id:number){
     this.wallet=e
+    this.id = id
+    //update du front
+    this.users[id-1].wallet=e
+    //supp des messages, erreur etc..
+    this.eraseMessage()
   }
-  send(){
-    this.UserService.sendValueWallet
-    
-  }
-  submitWallet(wallet,id){
-    this.user.wallet = wallet
-    this.user.id = id
-    this.send(this.user,this.user)
-    console.log(id)
+  submitWallet(newWallet:string){//entrée venant de l'input type text
+    console.log(typeof newWallet) //string
+     let newW = parseFloat(newWallet)
+    console.log(typeof newW)
+    if(isNaN(newW)){// entre toujours dans la condition
+      this.eraseMessage()
+      return this.message = `entrez un nombre et pas ${newWallet} svp`
+    }
+    if(this.wallet && this.id){
+      //requete ajax
+      this.wallet=newW
+      console.log(this.wallet, this.id)
+      //function updateList avec l'id
+      this.UserService.sendValueWallet(this.wallet, this.id,this.body).subscribe(
+        (body:Body)=>{
+          this.editUser(body.wallet, body.id)
+          console.log(body)
+          this.eraseMessage()
+          this.message = `la cagnotte de ${body.firstname} ${body.name} a bien été mise à jour `
+        },
+        error => {
+          // console.log(error)
+          this.errors = error;
+        }
+        )
+    }
   }
 }
--- src/app/user-list/user.service.ts
+++ src/app/user-list/user.service.ts
@@ -1,18 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-export interface User{
-  id:number
-  wallet:number
-}
+// export interface User{
+//   id:number
+//   wallet:number
+// }
 const API:string = "http://localhost:8080/lunchtime/user/findall"
 const API2:string ="http://localhost:8080/lunchtime/user/credit/"
+const API3:string = "http://localhost:8080/lunchtime/user/debit/"
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-
-  
-
 
   constructor(private http: HttpClient) { }
 
@@ -22,9 +20,15 @@ export class UserService {
   getUserList(){
     return this.http.get(API)
   }
-
-  sendValueWallet(user:User, wallet){
-    return this.http.post(API2 +user.id+"?amout="+user.wallet, wallet)
+  // setValue(wallet, id, body){
+  //   this.sendValueWallet(id,wallet,body)
+  // }
+  sendValueWallet(wallet:number, id:number,body){
+    if(wallet>0){
+    return this.http.post(API2 +id+"?amount="+wallet, body)
+    }else{
+    return this.http.post(API3 +id+"?amount="+Math.abs(wallet), body)
+    }
   }
 }
 
